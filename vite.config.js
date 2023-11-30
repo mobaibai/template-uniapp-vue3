ac09803c3e4c6b24203379fce4d1b88e5b422882
mobaibai <puresilent.com@gmail.com>
2023-11-30 15:40:17 +0800

    ✨ feat: 添加自动导入插件

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,22 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
 
-    // https://github.com/unocss/unocss
-    Unocss()
-  ],
+    Unocss(),
+
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-imports.d.ts',
+      // 用到 eslint 的配置
+      // eslintrc: {
+      //   enabled: true
+      // }
+    })
+  ]
 })
