82630f65fd4c7434d54b5a98a3982d9ec641a947
mobaibai <puresilent.com@gmail.com>
2023-11-30 15:24:54 +0800

    ✨ feat: Store的持久化配置兼容多端

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,7 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => ({ count: 0 }),
-  persist: true,
+  // 持久化配置：兼容多端
+  persist: {
+    storage: {
+      setItem(key, value) {
+        uni.setStorageSync(key, value)
+      },
+      getItem(key) {
+        return uni.getStorageSync(key)
+      }
+    }
+  },
   actions: {
     /**
      * @description: 增加Count
